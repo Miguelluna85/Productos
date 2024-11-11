5d579f94164ad7f34cecd97c31bd06a1d9803e3f
Miguelluna85 <85904166+Miguelluna85@users.noreply.github.com>
2024-11-11 00:24:14 -0600

    creo la migración a la db

--- Productos.Server/Program.cs
+++ Productos.Server/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Productos.Server.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Cadena de conexión
+builder.Services.AddDbContext<ProductosContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
