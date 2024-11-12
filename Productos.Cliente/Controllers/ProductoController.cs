0eaa8c05f007e647b79fcf592a4c90ac1015bf98
Miguelluna85 <85904166+Miguelluna85@users.noreply.github.com>
2024-11-12 17:19:00 -0600

    add views Details and edit

--- Productos.Cliente/Controllers/ProductoController.cs
+++ Productos.Cliente/Controllers/ProductoController.cs
@@ -22,7 +22,7 @@ public class ProductoController : Controller
         {
             string content = await response.Content.ReadAsStringAsync();
             IEnumerable<ProductoViewModel>? Productos = JsonConvert.DeserializeObject<IEnumerable<ProductoViewModel>>(content);
-            
+
             return View(Productos);
         }
         return View(new List<ProductoViewModel>());
@@ -39,7 +39,7 @@ public class ProductoController : Controller
         if (ModelState.IsValid)
         {
             string json = JsonConvert.SerializeObject(producto);
-            StringContent content = new StringContent(json, System.Text.Encoding.UTF8, 
+            StringContent content = new StringContent(json, System.Text.Encoding.UTF8,
                                                             "application/json");
             HttpResponseMessage response = await _httpClient
                                 .PostAsync("api/Productos/crear", content);
@@ -55,7 +55,76 @@ public class ProductoController : Controller
         return View(producto);
     }
 
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync($"api/Productos/ver?id={id}");
 
+        if (response.IsSuccessStatusCode)//code 200
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var producto = JsonConvert.DeserializeObject<ProductoViewModel>(content);
 
+            return View(producto);
+        }
+        else
+        {
+            return RedirectToAction("Details");
+        }       
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(int id, ProductoViewModel producto)
+    {
+        if (ModelState.IsValid)
+        {
+            var json = JsonConvert.SerializeObject(producto);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PutAsync($"/api/Productos/editar?id={id}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", new { id });
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty,"Error al actualizar producto");
+            }
+        }
+
+        return View(producto);
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync($"/api/Productos/ver?id={id}");
+        if (response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            ProductoViewModel? producto = JsonConvert.DeserializeObject<ProductoViewModel>(content);
+
+            return View(producto);
+        }
+        else
+        {
+            return RedirectToAction("Details");
+        }
+
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        HttpResponseMessage response = await _httpClient.DeleteAsync($"/api/Productos/eliminar?id={id}");
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction("index");
+        }
+        else
+        {
+            TempData["Error"] = "Error al eliminar producto";
+            return RedirectToAction("index");
+        }
+    }
 
 }
